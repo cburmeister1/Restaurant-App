42fccc5e3b55f83c25320dcae848cd148e7c55d4
DanSeiser <daniel.seiser@gmail.com>
2017-09-18 19:11:33 -0500

    Commit server changes

--- server.js
+++ server.js
@@ -15,72 +15,67 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Star Wars Characters (DATA)
+// PEOPLE DATA
 // =============================================================
-var characters = [{
-  routeName: "yoda",
-  name: "Yoda",
-  role: "Jedi Master",
-  age: 900,
-  forcePoints: 2000
-}, {
-  routeName: "darthmaul",
-  name: "Darth Maul",
-  role: "Sith Lord",
-  age: 200,
-  forcePoints: 1200
-}, {
-  routeName: "obiwankenobi",
-  name: "Obi Wan Kenobi",
-  role: "Jedi Master",
-  age: 55,
-  forcePoints: 1350
-}];
+
+var waitlist = [];
+var tables = [];
+
+function Person(reserve_name,reserve_phone,reserve_email,reserve_uniqueID){
+  this.reserve_name = reserve_name;
+  this.reserve_phone = reserve_reserve_phone;
+  this.reserve_email = reserve_email;
+  this.reserve_uniqueID = reserve_uniqueID;
+}
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+/*
 // Get all characters
 app.get("/all", function(req, res) {
   res.json(characters);
 });
+*/
 
-// Search for Specific Character (or all characters) - provides JSON
-app.get("/api/:characters?", function(req, res) {
-  var chosen = req.params.characters;
-
-  if (chosen) {
-    console.log(chosen);
-
-    for (var i = 0; i < characters.length; i++) {
-      if (chosen === characters[i].routeName) {
-        return res.json(characters[i]);
-      }
-    }
-    return res.json(false);
-  }
-  return res.json(characters);
+//return confirmed
+app.get("/api/tables", function(req, res) {
+  return res.json(tables);
+});
+//return waiting
+app.get("/api/waitlist", function(req, res) {
+  return res.json(waitlist);
 });
 
-// Create New Characters - takes in JSON input
+
+
+// Create New "Reservation" - takes in JSON input
 app.post("/api/new", function(req, res) {
-  var newcharacter = req.body;
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  var personData = req.body;
+  var newPerson = new Person(personData.reserve_name,personData.reserve_email,personData.reserve_phone,personData.reserve_uniqueID);
 
-  console.log(newcharacter);
+  if(confirmed.length <= 5){
+    confirmed.push(newPerson);
+  }else{
+    waiting.push(newPerson);
+  }
 
-  characters.push(newcharacter);
+  console.log(newPerson);
 
-  res.json(newcharacter);
+  res.json(newPerson);
 });
 
 // Starts the server to begin listening
